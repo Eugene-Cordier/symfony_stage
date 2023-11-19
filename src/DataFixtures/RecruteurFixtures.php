5cd6799bc8d09156647651e200414c4e83c53ee0
Daret Tom <daret.tom@gmail.com>
2023-11-19 22:01:18 +0100

    création de la forge et le générateur de recruteur

---
+++ src/DataFixtures/RecruteurFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\EntrepriseFactory;
+use App\Factory\RecruteurFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+;
+
+class RecruteurFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        RecruteurFactory::createMany(30, function () {
+            return [
+                'category' => EntrepriseFactory::random(),
+            ];
+        });
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            EntrepriseFixtures::class,
+        ];
+
+    }
+}
