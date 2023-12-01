b4176aaad3e83707620fa0f50ce1bf69d2796993
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-12-01 20:18:46 +0100

    Ajout de la méthode search dans la méthode index de PosteController.php

--- src/Controller/PosteController.php
+++ src/Controller/PosteController.php
@@ -4,18 +4,21 @@ namespace App\Controller;
 
 use App\Repository\PosteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PosteController extends AbstractController
 {
     #[Route('/poste', name: 'app_poste')]
-    public function index(PosteRepository $posteRepository): Response
+    public function index(PosteRepository $posteRepository, Request $request): Response
     {
-        $postes = $posteRepository->findAll();
+        $search = $request->query->get('search', '');
+        $postes = $posteRepository->search($search);
 
         return $this->render('poste/index.html.twig', [
             'postes' => $postes,
+            'search' => $search,
         ]);
     }
 }
