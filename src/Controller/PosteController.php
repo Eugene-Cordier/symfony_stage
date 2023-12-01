32a0c9a07134047b9cccdf57f8ddfb2aad7bd17f
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-01 18:58:30 +0100

    modification de la route index

--- src/Controller/PosteController.php
+++ src/Controller/PosteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PosteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class PosteController extends AbstractController
 {
     #[Route('/poste', name: 'app_poste')]
-    public function index(): Response
+    public function index(PosteRepository $posteRepository): Response
     {
+        $postes = $posteRepository->findAll();
+
         return $this->render('poste/index.html.twig', [
-            'controller_name' => 'PosteController',
+            'postes' => $postes,
         ]);
     }
 }
