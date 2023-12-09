6044313f8ed6c105dbb98533e4fe940362fe6235
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-09 18:13:21 +0100

    test: correction et optimisation méthode testSearch()

--- tests/Controller/PosteCest.php
+++ tests/Controller/PosteCest.php
@@ -2,38 +2,46 @@
 
 namespace App\Tests\Controller;
 
+use App\Factory\EntrepriseFactory;
 use App\Factory\PosteFactory;
+use App\Factory\TagFactory;
 use App\Tests\Support\ControllerTester;
+
+use function PHPUnit\Framework\assertEquals;
 
 class PosteCest
 {
     public function testSearch(ControllerTester $I)
     {
-        //création d'une entité
-        $datedeb = PosteFactory::faker()->dateTimeBetween('+3 month', '+1 year');
-        $datefin =
-        PosteFactory::createOne(['date_deb' => \DateTime::createFromFormat('Y-m-d', '2024-11-24'),
+        // création d'une entité
+        $entreprise = EntrepriseFactory::createOne(['addresse' => 'Paris', 'nom' => 'sbire']);
+        $tag = TagFactory::createOne(['description' => 'blalbla', 'nom' => 'DEV_WEB']);
+        $date_deb = \DateTime::createFromFormat('Y-m-d', '2024-11-24');
+        $date_fin = \DateTime::createFromFormat('Y-m-d', '2025-02-12');
+        PosteFactory::createOne(['date_deb' => $date_deb,
             'description' => 'blabla',
-            'entreprise' => null,
+            'entreprise' => $entreprise,
             'label' => 'alternance',
             'lieu' => 'Paris',
-            'tag' => null,
-            'date_fin' => \DateTime::createFromFormat('Y-m-d', '2025-02-12'),
+            'tag' => $tag,
+            'date_fin' => $date_fin,
         ]);
         // remplissage formulaire et recherche
-        $I->amOnPage('/site');
-        $I->fillField("//input[@type='search']", 'alternance');
-        $I->click('Rechercher');
-        //vérification résultat
-        $I->seeCurrentUrlEquals('http://localhost:8000/poste?search=alternance');
-        $I->see('alternance');
-        $I->see('blabla');
+        $I->amOnPage('/poste?search=DEV_WEB');
+        // $I->fillField("//input[@type='search']", 'DEV_WEB');
+        // $I->click('search-button');
+        // vérification résultat
+        // $I->seeCurrentRouteIs('app_poste',['search'=>'DEV_WEB']);
+        // $I->amOnPage('/poste');
+        assertEquals(['alternance', 'DEV_WEB', 'Paris', 'sbire', "24/11/2024 - 12/02/2025", 'blabla'], $I->grabMultiple('.alternance>ul>li'));
+        // $I->see('DEV_WEB');
+        // $I->see('blabla');
     }
 
     // tests
     public function testPostePage(ControllerTester $I)
     {
-        //création d'une entité
+        // création d'une entité
         $datedeb = PosteFactory::faker()->dateTimeBetween('+3 month', '+1 year');
         $datefin =
             PosteFactory::createOne(['date_deb' => \DateTime::createFromFormat('Y-m-d', '2024-11-24'),
@@ -44,8 +52,8 @@ class PosteCest
                 'tag' => null,
                 'date_fin' => \DateTime::createFromFormat('Y-m-d', '2025-02-12'),
             ]);
-        //vérification de la page et d'une entitée
-        $I->amOnRoad("/poste");
+        // vérification de la page et d'une entitée
+        $I->amOnRoad('/poste');
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle("Offres d'emploi basées sur votre recherche");
         $I->see('alternance');
