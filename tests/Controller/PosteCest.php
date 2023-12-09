4c849f827afec1b33717b13f9dbb886184f706a3
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-09 18:38:57 +0100

    test: PosteCest methode testPostPage(), (inversement position des fonction dans le fichier)

--- tests/Controller/PosteCest.php
+++ tests/Controller/PosteCest.php
@@ -11,6 +11,18 @@ use function PHPUnit\Framework\assertEquals;
 
 class PosteCest
 {
+    // tests
+    public function testPostePage(ControllerTester $I)
+    {
+        // création d'une entité
+        PosteFactory::createMany(20);
+        $I->amOnPage('/poste');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle("Offres d'emploi basées sur votre recherche");
+        $I->seeNumberOfElements("ul ul",20);
+        $I->seeNumberOfElements("ul ul>li",20*6);
+
+    }
     public function testSearch(ControllerTester $I)
     {
         // création d'une entité
@@ -36,27 +48,5 @@ class PosteCest
         assertEquals(['alternance', 'DEV_WEB', 'Paris', 'sbire', "24/11/2024 - 12/02/2025", 'blabla'], $I->grabMultiple('.alternance>ul>li'));
         // $I->see('DEV_WEB');
         // $I->see('blabla');
-    }
-
-    // tests
-    public function testPostePage(ControllerTester $I)
-    {
-        // création d'une entité
-        $datedeb = PosteFactory::faker()->dateTimeBetween('+3 month', '+1 year');
-        $datefin =
-            PosteFactory::createOne(['date_deb' => \DateTime::createFromFormat('Y-m-d', '2024-11-24'),
-                'description' => 'blabla',
-                'entreprise' => null,
-                'label' => 'alternance',
-                'lieu' => 'Paris',
-                'tag' => null,
-                'date_fin' => \DateTime::createFromFormat('Y-m-d', '2025-02-12'),
-            ]);
-        // vérification de la page et d'une entitée
-        $I->amOnRoad('/poste');
-        $I->seeResponseCodeIsSuccessful();
-        $I->seeInTitle("Offres d'emploi basées sur votre recherche");
-        $I->see('alternance');
-        $I->see('blabla');
     }
 }
