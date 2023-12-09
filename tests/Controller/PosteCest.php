c4c1708b21c3e3dab7aec26d97d7d9f8eca13e38
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-09 16:59:32 +0100

    test: testSearch() correction du type des dates_deb/fin+ commentaires

--- tests/Controller/PosteCest.php
+++ tests/Controller/PosteCest.php
@@ -9,17 +9,22 @@ class PosteCest
 {
     public function testSearch(ControllerTester $I)
     {
-        PosteFactory::createOne(['date_deb' => '22/11/2024',
+        //création d'une entité
+        $datedeb = PosteFactory::faker()->dateTimeBetween('+3 month', '+1 year');
+        $datefin =
+        PosteFactory::createOne(['date_deb' => \DateTime::createFromFormat('Y-m-d', '2024-11-24'),
             'description' => 'blabla',
             'entreprise' => null,
             'label' => 'alternance',
             'lieu' => 'Paris',
             'tag' => null,
-            'date_fin' => '22/01/2025',
+            'date_fin' => \DateTime::createFromFormat('Y-m-d', '2025-02-12'),
         ]);
+        // remplissage formulaire et recherche
         $I->amOnPage('/site');
         $I->fillField("//input[@type='search']", 'alternance');
         $I->click('Rechercher');
+        //vérification résultat
         $I->seeCurrentUrlEquals('http://localhost:8000/poste?search=alternance');
         $I->see('alternance');
         $I->see('blabla');
