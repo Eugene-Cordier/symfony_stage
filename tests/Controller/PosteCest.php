351d0da518be7efc65564c4d97b1fe0e6c705912
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-09 16:20:18 +0100

    test: testSearch() methode

--- tests/Controller/PosteCest.php
+++ tests/Controller/PosteCest.php
@@ -1,14 +1,28 @@
 <?php
 
-
 namespace App\Tests\Controller;
 
+use App\Factory\PosteFactory;
 use App\Tests\Support\ControllerTester;
 
 class PosteCest
 {
-    public function _before(ControllerTester $I)
+    public function testSearch(ControllerTester $I)
     {
+        PosteFactory::createOne(['date_deb' => '22/11/2024',
+            'description' => 'blabla',
+            'entreprise' => null,
+            'label' => 'alternance',
+            'lieu' => 'Paris',
+            'tag' => null,
+            'date_fin' => '22/01/2025',
+        ]);
+        $I->amOnPage('/site');
+        $I->fillField("//input[@type='search']", 'alternance');
+        $I->click('Rechercher');
+        $I->seeCurrentUrlEquals('http://localhost:8000/poste?search=alternance');
+        $I->see('alternance');
+        $I->see('blabla');
     }
 
     // tests
