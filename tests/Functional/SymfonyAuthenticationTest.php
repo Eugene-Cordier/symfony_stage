bfc8f69a4d9fcef16075086b64989df43ed6898d
Daret Tom <daret.tom@gmail.com>
2023-12-11 13:32:41 +0100

    création du fichier de test

---
+++ tests/Functional/SymfonyAuthenticationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests\Functional;
+
+use App\Tests\Support\FunctionalTester;
+
+class SymfonyAuthenticationTest extends \Codeception\Test\Unit
+{
+
+    protected FunctionalTester $tester;
+
+
+}
