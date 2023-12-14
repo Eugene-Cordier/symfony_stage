8d8fff94eef9d2e66e6c3fe415f151e3b538851e
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-12-14 15:42:07 +0100

    Ajout d'une redirection vers login si on essai d'acceder a la profil et que nous sommes pas connecté

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Etudiant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ProfilController extends AbstractController
 {
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
