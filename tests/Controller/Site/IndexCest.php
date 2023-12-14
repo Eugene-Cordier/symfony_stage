2fa73188236e4a78f4e0f5ca7cd73645a6f91bac
Daret Tom <daret.tom@gmail.com>
2023-12-14 20:42:09 +0100

    test de la liste de la listes des stage, test ok

--- tests/Controller/Site/IndexCest.php
+++ tests/Controller/Site/IndexCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\Site;
 
+use App\Factory\PosteFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -19,5 +20,14 @@ class IndexCest
     {
         $I->amOnPage('/site');
         $I->seeResponseCodeIsSuccessful();
+    }
+
+    public function testPageStage(ControllerTester $I)
+    {
+        PosteFactory::createMany(5);
+        $I->amOnPage('/poste');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle("Offres d'emploi basées sur votre recherche");
+        $I->seeNumberOfElements('ul.alternance, ul.stage', 2);
     }
 }
