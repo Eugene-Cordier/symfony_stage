2cf82b2b42fd306615afb21cfeeaa850cc5c1ca4
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-18 18:36:14 +0100

    Fixtures: ajout dependances et utilisation de posts_descriptions.json dans PosteFixtures.php

--- src/DataFixtures/PosteFixtures.php
+++ src/DataFixtures/PosteFixtures.php
@@ -8,24 +8,27 @@ use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class PosteFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         PosteFactory::createMany(30, function () {
+            $file_content = file_get_contents(__DIR__.'/data/posts_descriptions.json');
+            $data = json_decode($file_content, true);
             return [
                 'entreprise' => EntrepriseFactory::random(),
                 'tag' => TagFactory::random(),
+                'description' => $data[random_int(0,count($data)-1)]['description'],
             ];
         });
     }
+
     public function getDependencies(): array
     {
         return [
             TagFixtures::class,
+            EntrepriseFixtures::class,
         ];
-
     }
 }
