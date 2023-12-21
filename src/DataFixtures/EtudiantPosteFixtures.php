4107c7271541c808979ca2f0f898067bb0f7d0ce
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-12-21 10:24:12 +0100

    ajout de la dépendance sur la fixture

--- src/DataFixtures/EtudiantPosteFixtures.php
+++ src/DataFixtures/EtudiantPosteFixtures.php
@@ -5,12 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\EtudiantPoste;
 use App\Factory\EtudiantPosteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class EtudiantPosteFixtures extends Fixture
+class EtudiantPosteFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         EtudiantPosteFactory::createMany(10);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            EtudiantFixtures::class,
+            PosteFixtures::class,
+        ];
     }
 }
