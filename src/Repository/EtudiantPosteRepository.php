e25452e975e4db27b083a134c848301a35ffe5a2
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-12-21 09:44:31 +0100

    supression des lignes inutiles dans le repository

--- src/Repository/EtudiantPosteRepository.php
+++ src/Repository/EtudiantPosteRepository.php
@@ -20,29 +20,4 @@ class EtudiantPosteRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, EtudiantPoste::class);
     }
-
-//    /**
-//     * @return EtudiantPoste[] Returns an array of EtudiantPoste objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?EtudiantPoste
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
