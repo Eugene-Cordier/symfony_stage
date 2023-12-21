de98c44f7dbe4b974e5ad8463977db800eeafacf
Tom Daret <daret.tom@gmail.com>
2023-12-21 10:27:54 +0100

    modification de fixtures

--- src/DataFixtures/TagFixtures.php
+++ src/DataFixtures/TagFixtures.php
@@ -5,17 +5,22 @@ namespace App\DataFixtures;
 use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class TagFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        TagFactory::createSequence(
-            json_decode(
-                file_get_contents(__DIR__.'/data/tag.json'),
-                true
-            )
-        );
+        $tags = json_decode(file_get_contents(__DIR__.'/data/tag.json'), true);
+        $tag = [];
+        for ($i = 0; $i < count($tags); $i = $i + 2) {
+            $tag[] = [$tags[$i], $tags[$i + 1]];
+        }
+        foreach ($tag as $t) {
+            TagFactory::createSequence(
+                [
+                    ['nom' => $t[0]['nom'], 'description' => $t[1]['description']],
+                ]
+            );
+        }
     }
 }
