f756d328e609dfc3f78d597218c6ac86373bc32d
Daret Tom <daret.tom@gmail.com>
2024-01-03 10:45:31 +0100

    modification des paramètres sur le dashboard et sur les formulaires

--- src/Controller/Admin/EtudiantCrudController.php
+++ src/Controller/Admin/EtudiantCrudController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Etudiant;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class EtudiantCrudController extends AbstractCrudController
 {
@@ -15,14 +17,23 @@ class EtudiantCrudController extends AbstractCrudController
         return Etudiant::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+            IdField::new('id')->hideOnForm(),
+            TextField::new('prenom'),
+            TextField::new('nom'),
+            TextField::new('login'),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->onlyOnForms()
+                ->setFormType(PasswordType::class)
+                ->setFormTypeOptions([
+                    'required' => false,
+                    'empty_data' => '',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ]),
+            ];
+
     }
-    */
 }
