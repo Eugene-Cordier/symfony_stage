4e369d7128345a224288e0199e321f2f244bbf60
eugene <eugene.cordier@etudiant.univ-reims.fr>
2024-01-09 20:15:22 +0100

    ajout hideOnForm() sur l'id

--- src/Controller/Admin/TagCrudController.php
+++ src/Controller/Admin/TagCrudController.php
@@ -20,7 +20,7 @@ class TagCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('nom'),
             TextEditorField::new('description'),
             AssociationField::new('postes')->setFormTypeOptions(
