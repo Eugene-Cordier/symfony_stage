87907621bfad535b6e4a58581124e56f9ae8594b
Ferdi SAHIN <ferdi.sahin@etudiant.univ-reims.fr>
2024-01-09 10:45:44 +0100

    Configuration du crud de Administrateur

--- src/Controller/Admin/AdministrateurCrudController.php
+++ src/Controller/Admin/AdministrateurCrudController.php
@@ -3,26 +3,65 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Administrateur;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AdministrateurCrudController extends AbstractCrudController
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Administrateur::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('prenom'),
+            TextField::new('nom'),
+            TextField::new('login'),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->onlyOnForms()
+                ->setFormType(PasswordType::class)
+                ->setFormTypeOptions([
+                    'required' => false,
+                    'empty_data' => '',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ]),
         ];
     }
-    */
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->setUserPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function setUserPassword($entityInstance): void
+    {
+        $password = $this->getContext()->getRequest()->get('Administrateur')['password'];
+        dump($password);
+        if (!empty($password)) {
+            $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        }
+    }
 }
