8b0d5af7d779a26b32bf055781ce22bcb17bb0eb
Thomas PAZZÉ <thomas.pazze@etudiant.univ-reims.fr>
2024-01-11 03:59:23 +0100

    configuration du configureFields.

--- src/Controller/Admin/RecruteurCrudController.php
+++ src/Controller/Admin/RecruteurCrudController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Recruteur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RecruteurCrudController extends AbstractCrudController
 {
@@ -15,14 +17,15 @@ class RecruteurCrudController extends AbstractCrudController
         return Recruteur::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('prenom'),
+            TextField::new('nom'),
+            TextField::new('login'),
+            EmailField::new('email'),
         ];
     }
-    */
+
 }
