07470e07f91ea77bfaab6fa78b00fdb4163256c6
Thomas PAZZÉ <thomas.pazze@etudiant.univ-reims.fr>
2024-01-11 04:44:21 +0100

    suppréssion de l'import User.

--- src/Controller/Admin/RecruteurCrudController.php
+++ src/Controller/Admin/RecruteurCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recruteur;
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
