06e8d3f9354092683c83161487fb2771002ac343
Thomas PAZZÉ <thomas.pazze@etudiant.univ-reims.fr>
2024-01-11 04:03:55 +0100

    ajout de la méthode setUserPassword.

--- src/Controller/Admin/RecruteurCrudController.php
+++ src/Controller/Admin/RecruteurCrudController.php
@@ -3,10 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recruteur;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -19,6 +19,7 @@ class RecruteurCrudController extends AbstractCrudController
     {
         $this->passwordHasher = $passwordHasher;
     }
+
     public static function getEntityFqcn(): string
     {
         return Recruteur::class;
@@ -41,6 +42,13 @@ class RecruteurCrudController extends AbstractCrudController
                     'attr' => ['autocomplete' => 'new-password'],
                 ]),
         ];
+    }
+    public function setUserPassword($entityInstance): void
+    {
+        $password = $this->getContext()->getRequest()->get('Recruteur')['password'];
+        if (!empty($password)) {
+            $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        }
     }
 
 }
